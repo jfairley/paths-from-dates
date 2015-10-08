0bd016c4ea4a254ba1ea9c5d07ffb7433454e3a7
Jeff Fairley <jfairley@gmail.com>
2015-10-08 02:04:33 -0600

    process one file at a time

--- server.js
+++ server.js
@@ -1,75 +1,75 @@
 var mediainfo = require('mediainfo');
-var prompt = require('prompt');
 var Q = require('q');
 var fs = require('fs');
 var FS = require('q-io/fs');
 var moment = require('moment');
 var path = require('path');
 
-//
-// Start the prompt
-//
-prompt.start();
 
-//
-// Get two properties from the user: input and output
-//
-Q.nfcall(prompt.get, ['input', 'output'])
-    .then(function (result) {
-        console.log('Command-line input received:');
-        console.log('  input:  ' + result.input);
-        console.log('  output: ' + result.output);
+var args = process.argv.slice(2);
+if (args.length < 2) {
+    console.log('input args', args);
+    throw 'ERROR: expect 2 args: input and output path';
+}
 
-        function validatePath(path) {
-            return FS.exists(path)
-                .then(function (exists) {
-                    if (exists === false) {
-                        throw path + ' path does not exist';
-                    }
-                    return FS.isDirectory(path)
-                })
-                .then(function (isDirectory) {
-                    if (isDirectory === false) {
-                        throw path + ' path is not a directory';
-                    }
-                });
-        }
+var inputBaseDir = path.resolve(args[0]);
+var outputBaseDir = path.resolve(args[1]);
 
-        return Q.all([
-            validatePath(result['input']),
-            validatePath(result['output'])
-        ]).then(function () {
-            return [result.input, result.output];
+function validatePath(path) {
+    if (!fs.existsSync(path)) {
+        throw path + ' path does not exist';
+    }
+    if (!fs.statSync(path).isDirectory()) {
+        throw path + ' path is not a directory';
+    }
+}
+validatePath(inputBaseDir);
+validatePath(outputBaseDir);
+
+console.log('reading tree from input path', inputBaseDir);
+
+return FS.listTree(inputBaseDir)
+    .then(function (inputPaths) {
+        return inputPaths.filter(function (path) {
+            return !fs.statSync(path).isDirectory();
         });
     })
-    .spread(function (input, output) {
-        console.log('reading tree from input path');
-        return FS.listTree(input)
-            .then(function (inputPaths) {
-                return Q.all(inputPaths
-                    .filter(function (inputPath) {
-                        return !fs.statSync(inputPath).isDirectory();
-                    })
-                    .map(function (inputPath) {
-                        return Q.nfcall(mediainfo, inputPath)
-                            .then(function (result) {
-                                var dateString = result.File_last_modification_date;
-                                var m = moment.utc(dateString);
-                                var outputDir = path.resolve(output, m.year(), m.month(), m.date());
-                                return FS.makeTree(outputDir)
-                                    .then(function () {
-                                        return FS.base(inputPath);
-                                    })
-                                    .then(function (inputBase) {
-                                        var outputPath = path.resolve(outputDir, inputBase);
-                                        return FS.copy(inputPath, outputPath)
-                                            .then(function () {
-                                                console.log('copied %s to %s', inputPath, outputPath);
-                                            });
-                                    });
-                            });
-                    }));
-            });
+    .then(function (inputPaths) {
+        return inputPaths.reduce(function (promise, inputPath) {
+            console.log(typeof inputPath, inputPath);
+            return promise
+                .then(function () {
+                    return Q.nfcall(mediainfo, inputPath);
+                })
+                .then(function (result) {
+                    var outputDir = outputBaseDir;
+                    var dateString = result.File_last_modification_date;
+                    if (dateString) {
+                        var m = moment.utc(dateString);
+                        if (m.isValid()) {
+                            outputDir = path.resolve(outputBaseDir, m.year(), m.month(), m.date());
+                        } else {
+                            console.error('cannot read date', dateString, JSON.stringify(result));
+                        }
+                    } else {
+                        console.error('cannot find date', JSON.stringify(result));
+                    }
+                    return FS.makeTree(outputDir)
+                        .then(function () {
+                            return FS.base(inputPath);
+                        })
+                        .then(function (inputFilename) {
+                            var outputPath = path.resolve(outputDir, inputFilename);
+                            return FS.copy(inputPath, outputPath)
+                                .then(function () {
+                                    console.log('copied %s to %s', inputPath, outputPath);
+                                });
+                        });
+                });
+        }, Q());
+    })
+    .then(function () {
+        console.log('this is after everything');
     })
     .catch(function (err) {
         console.error(err.toString());
