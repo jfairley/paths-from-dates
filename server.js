50642e98fd61d679d9be39dfc30bbb11497ab28b
Jeff Fairley <jfairley@gmail.com>
2015-10-07 23:53:03 -0600

    with promise

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var mediainfo = require('mediainfo');
 var prompt = require('prompt');
-var fs = require('fs');
 var Q = require('q');
+var FS = require('q-io/fs');
 
 //
 // Start the prompt
@@ -11,33 +11,33 @@ prompt.start();
 //
 // Get two properties from the user: input and output
 //
-prompt.get(['input', 'output'], function (err, result) {
-    console.log('Command-line input received:');
-    console.log('  input:  ' + result.input);
-    console.log('  output: ' + result.output);
+Q.nfcall(prompt.get, ['input', 'output'])
+    .then(function (result) {
+        console.log('Command-line input received:');
+        console.log('  input:  ' + result.input);
+        console.log('  output: ' + result.output);
 
-    var failure = false;
-
-    function validatePath(result, prop) {
-        if (!fs.existsSync(result[prop])) {
-            console.error(prop, 'path does not exist');
-            failure = true;
-        } else {
-            var stat = fs.statSync(result[prop]);
-            if (!stat.isDirectory()) {
-                console.error(prop, 'path is not a directory');
-                failure = true;
-            }
+        function validatePath(path) {
+            return FS.exists(path)
+                .then(function (exists) {
+                    if (exists === false) {
+                        throw path + ' path does not exist';
+                    }
+                    return FS.isDirectory(path)
+                })
+                .then(function (isDirectory) {
+                    if (isDirectory === false) {
+                        throw path + ' path is not a directory';
+                    }
+                });
         }
-    }
 
-    validatePath(result, 'input');
-    validatePath(result, 'output');
-
-    if (failure) {
+        return Q.all([
+            validatePath(result['input']),
+            validatePath(result['output'])
+        ]);
+    })
+    .catch(function (err) {
+        console.error(err);
         process.exit(1);
-        return;
-    }
-
-
-});
+    });
